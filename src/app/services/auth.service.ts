fbd12dcc6b276ec614bb9b77a61c415a818ff2fa
Coy Carlson <corycarlsonlca@gmail.com>
2019-10-16 18:00:58 -0500

    adds logon.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,14 +1,45 @@
 import { Injectable } from "@angular/core";
 import { IUser } from "../interfaces/iuser";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { Observable, BehaviorSubject } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root"
 })
 export class AuthService {
-  currentUser: IUser;
-  constructor(private http: HttpClient) {}
+  private currentUserSubject: BehaviorSubject<IUser>;
+  public currentUser: Observable<IUser>;
+
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<IUser>(
+      JSON.parse(localStorage.getItem("currentUser"))
+    );
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): IUser {
+    return this.currentUserSubject.value;
+  }
+
+  login(username: string, password: string) {
+    return this.http
+      .post<any>(`/users/authenticate`, { username, password })
+      .pipe(
+        map(user => {
+          // store user details and jwt token in local storage to keep user logged in between page refreshes
+          localStorage.setItem("currentUser", JSON.stringify(user));
+          this.currentUserSubject.next(user);
+          return user;
+        })
+      );
+  }
+
+  logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem("currentUser");
+    this.currentUserSubject.next(null);
+  }
 
   register(user: any): Observable<IUser> {
     return this.http.post<IUser>(
